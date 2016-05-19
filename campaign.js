a0f29dfb9fd7dd8c445fcc8f136596b0be8acd4b
N.Shimizu <chikoski@gmail.com>
2016-05-19 09:33:21 +0900

    initial content

---
+++ campaign.js
@@ -0,0 +1,13 @@
+const FEMALE = 0;
+const MALE = 1;
+const MISC = 2;
+const MARRIED = true;
+const UNMARRIED = false;
+
+const CAMPAIGN_MARRIED_MALE = 1;
+const CAMPAIGN_UNMARRIED_FEMALE = 2;
+const CAMPAIGN_MARRIED_FEMALE = 3;
+const CAMPAIGN_OTHERS = 4;
+
+function campaignNumber(age, sex, married){
+}
